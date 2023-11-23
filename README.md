f8d09c3311942738af9771f3dd2475c017026436
Peter Rush (Psychology) <57416249+pxr687@users.noreply.github.com>
2023-11-23 17:33:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A function to display a pandas dataframe in the browswer, to be run from the
+command line, for better visualisation than viewing within the terminal.
---
+++ show_df.py
@@ -0,0 +1,20 @@
+import webbrowser
+import os
+from time import sleep
+
+def show_df(df, name='temp', delete=True):
+    """Show a pandas dataframe in browser, from command line."""
+    # convert the dataframe to HTML
+    html_df = df.to_html()
+
+    # write a temporary HTML file
+    with open(name+'.html', 'w') as file:
+        file.write(html_df)
+
+    # display the dataframe in browswer
+    webbrowser.open(file.name)
+
+    # delete the temporary file (after a pause to allow display in browswer)
+    if delete == True:
+        sleep(2)
+        os.remove(name+'.html')
