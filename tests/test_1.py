8a5fbbc0f8cead82f3c8393443666e15c0ed734d
Peter Rush (Psychology) <57416249+pxr687@users.noreply.github.com>
2023-11-23 22:42:50 +0000

    update test

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,11 +4,12 @@ import numpy as np
 
 def test_1():
     # create dataframe
-    df = pd.DataFrame({'score': np.random.normal(100, 10, 100),
-                    'name': np.repeat('name', 100)})
+    df = pd.DataFrame({'score_1': np.random.normal(100, 10, 100),
+                    'score_2': np.random.normal(1000, 2, 100),
+                    'name': np.repeat(['A', 'B'], 50)})
 
     # test with a DataFrame
     show_df(df)
 
     # test with a Series
-    show_df(df['score'])
+    show_df(df.groupby('name').mean())
